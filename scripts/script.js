1a939759e27314597059ca09106d6bbea389aeef
Dusty <dustinluck@gmail.com>
2021-09-07 19:10:03 -0400

    added an html user form, a submit event listener with corresponding function, and in the process of debugging the setBlockLimit function

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,50 @@
 const container = document.querySelector('.container');
-const blocks = document.querySelectorAll('.block');
-const numberofBlocks = 256;
-let blockDiv;
+const block = document.querySelector('div');
+const form = document.querySelector('form');
+const quantity = document.querySelector('.quantity');
+const refresh = document.querySelector('button');
+const prompt = document.querySelector('p');
+const userInput = quantity.value;
 
-for (let i = 0; i < numberofBlocks; i++) {
-  blockDiv = document.createElement('div');
-  blockDiv.className = 'block';
-  container.append(blockDiv);
+container.style.setProperty(
+  'grid-template-columns', 
+  `repeat(${userInput}, 1fr)`
+  );
+
+container.style.setProperty(
+  'grid-template-rows', 
+  `repeat(${userInput}, 1fr)`
+  );
+
+function createBlockGrid () {
+  const numberofBlocks = userInput * userInput;
+  for (let i = 0; i < numberofBlocks; i++) {
+    let blockDiv = document.createElement('div');
+    blockDiv.className = 'block';
+    container.append(blockDiv);
+  }
 }
 
-blocks.forEach(block => {
-  block.addEventListener('mouseenter', () => console.log('Mouse enter'));
-  block.addEventListener('mouseleave', () => console.log('Mouse leave'));
-})
+function setBlockLimit () {
+  if (userInput > 100 || userInput < 1) {
+    userInput = 1;
+    quantity.value = 1;
+    return prompt.textContent = 'No good! Please enter a number between 1 \
+    and 100.';
+  }
+}
+
+function updateBlockGrid (e) {
+  location.reload();
+  prompt.textContent = 'LOADING';
+  e.preventDefault();
+}
+
+block.addEventListener('mouseover', function (e) {
+  e.target.classList.replace('block', 'color');
+});
+
+form.addEventListener('submit', updateBlockGrid);
+
+setBlockLimit(userInput);
+createBlockGrid();
