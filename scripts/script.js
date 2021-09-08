916016cfee1ae2cf295eefbe9c47ae216aed38a7
Dusty <dustinluck@gmail.com>
2021-09-08 13:39:48 -0400

    removed the html form + submit event listener and put container property methods inside createBlockGrid

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,48 @@
 const container = document.querySelector('.container');
 const block = document.querySelector('div');
-const form = document.querySelector('form');
 const quantity = document.querySelector('.quantity');
 const refresh = document.querySelector('button');
 const prompt = document.querySelector('p');
-const userInput = quantity.value;
-
-container.style.setProperty(
-  'grid-template-columns', 
-  `repeat(${userInput}, 1fr)`
-  );
-
-container.style.setProperty(
-  'grid-template-rows', 
-  `repeat(${userInput}, 1fr)`
-  );
+let userInput = quantity.value;
 
 function createBlockGrid () {
   const numberofBlocks = userInput * userInput;
+  container.style.setProperty(
+    'grid-template-columns', 
+    `repeat(${userInput}, 1fr)`
+    );
+
+  container.style.setProperty(
+    'grid-template-rows', 
+    `repeat(${userInput}, 1fr)`
+    );
+
   for (let i = 0; i < numberofBlocks; i++) {
     let blockDiv = document.createElement('div');
     blockDiv.className = 'block';
     container.append(blockDiv);
   }
+  setBlockLimit(userInput);
 }
 
 function setBlockLimit () {
-  if (userInput > 100 || userInput < 1) {
+  if (userInput > 100) {
+    userInput = 100;
+    quantity.value = 100;
+    prompt.textContent = 'Too high! Please enter a number between 1 \
+    and 100.';
+  } else if (userInput < 1) {
     userInput = 1;
     quantity.value = 1;
-    return prompt.textContent = 'No good! Please enter a number between 1 \
+    prompt.textContent = 'Too low! Please enter a number between 1 \
     and 100.';
   }
-}
-
-function updateBlockGrid (e) {
-  location.reload();
-  prompt.textContent = 'LOADING';
-  e.preventDefault();
 }
 
 block.addEventListener('mouseover', function (e) {
   e.target.classList.replace('block', 'color');
 });
 
-form.addEventListener('submit', updateBlockGrid);
+refresh.addEventListener('click', () => location.reload());
 
-setBlockLimit(userInput);
 createBlockGrid();
