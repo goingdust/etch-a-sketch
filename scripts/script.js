16a2fc5e1b669a98ba8b2f3f19e898cecd83770d
Dusty <dustinluck@gmail.com>
2021-09-08 21:43:56 -0400

    imported fonts, made color variables, styled some elements, began writing js to change blocks into random colours when hovered over

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
-const container = document.querySelector('.container');
-const block = document.querySelector('div');
+const gridContainer = document.querySelector('.container');
+const blocks = document.querySelector('div');
 const quantity = document.querySelector('.quantity');
 const refresh = document.querySelector('.refresh');
 const prompt = document.querySelector('.prompt');
 let userInput = quantity.value;
 
-function createBlockGrid () {
+function createBlockGrid() {
   const numberofBlocks = userInput * userInput;
-  container.style.setProperty(
+  gridContainer.style.setProperty(
     'grid-template-columns', 
     `repeat(${userInput}, 1fr)`
     );
 
-  container.style.setProperty(
+  gridContainer.style.setProperty(
     'grid-template-rows', 
     `repeat(${userInput}, 1fr)`
     );
@@ -20,11 +20,11 @@ function createBlockGrid () {
   for (let i = 0; i < numberofBlocks; i++) {
     let blockDiv = document.createElement('div');
     blockDiv.className = 'block';
-    container.append(blockDiv);
+    gridContainer.append(blockDiv);
   }
 }
 
-function setBlockLimit () {
+function setBlockLimit() {
   if (userInput > 100 || userInput < 1 || userInput == null) {
     userInput = 16;
     quantity.value = 16;
@@ -33,8 +33,28 @@ function setBlockLimit () {
   }
 }
 
-block.addEventListener('mouseover', function (e) {
+function generateRandomColor() {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  let randomColor = "rgb(" + x + "," + y + "," + z + ")";
+  return randomColor;
+}
+/*
+blocks.addEventListener('mouseover', function (e) {
   e.target.classList.replace('block', 'color');
+}); */
+
+// To change the blocks into random colours!
+blocks.classList.append('random-color');
+const randomColorClass = document.querySelector('.random-color');
+randomColorClass.style.setProperty(
+  'background-color',
+  `${generateRandomColor()}`
+);
+
+blocks.addEventListener('mouseover', function (e) {
+  e.target.classList.replace('block', 'random-color');
 });
 
 refresh.addEventListener('click', () => location.reload());
